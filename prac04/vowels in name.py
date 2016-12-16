6c29e4d6b5334905dd23796095e741cc154c6baf
jc449735 <upendrabthapa@my.jcu.edu.au>
2016-12-16 16:56:11 +1000

    all the practicals done in the programming I course

--- prac04/vowels in name.py
+++ prac04/vowels in name.py
@@ -1,4 +1,10 @@
 __author__ = 'jc449735'
-name=input("enter your lovely yet sacrastic and ironic name")
+name = input("enter your lovely yet sarcastic and ironic name: ")
+vowels = ["a","e","i","o","u"]
+vowel=0
+for char in name:
+    if char.lower() in vowels:
+        vowel += 1
 
-print(name[5])
+#print(vowel)
+print("Out of {} letters, {} has {} vowels".format(len(name),name,vowel))
