ea241eb8bfca3c448a19c247ed7bc1af140e5784
jc449735 <upendrabthapa@my.jcu.edu.au>
2016-12-16 21:33:42 +1000

    all the practicals done in the programming I course

--- prac04/ask for number and store that in file.py
+++ prac04/ask for number and store that in file.py
@@ -3,5 +3,8 @@ num=0
 temp_file=open("convert number in","w")
 while num >= 0:
     num=int(input("NUMBER :"))
-    print(str(num),file=temp_file)
+    if num >= 0:
+        print(str(num),file=temp_file)
+    else:
+            print("input only positive integer value")
 temp_file.close()
