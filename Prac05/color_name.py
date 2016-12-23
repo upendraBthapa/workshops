ad28af1dd08c74041f43b9f94078d8166a8ef242
jc449735 <upendrabthapa@my.jcu.edu.au>
2016-12-23 21:13:32 +1000

    all the practicals done in the programming I course

--- Prac05/color_name.py
+++ Prac05/color_name.py
@@ -1,6 +1,6 @@
 __author__ = 'jc449735'
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-COLOR_NAMES = {"DARKGREEN": "#006400", "DARKKHAKI": "#bdb76b", "DARKOLIVEGREEN": "#556b2f", "DarkOliveGreen1": "#caff70", "DarkOliveGreen2": "#bcee68", "DarkOliveGreen3": "#a2cd5a"}
+COLOR_NAMES = {"DARKGREEN": "#006400", "DARKKHAKI": "#bdb76b", "DARKOLIVEGREEN": "#556b2f", "DARKOLIVEGREEN1": "#caff70", "DARKOLIVEGREEN2": "#bcee68", "DARKOLIVEGRREEN3": "#a2cd5a"}
 # print(COLOR_NAMES)
 
 color = input("Enter short color: ").upper()
